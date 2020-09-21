91d4b06e75250445cfbe1c5b5b46c84eb32529a0
Deepthi Chokka <dtchokka@gmail.com>
2020-09-20 21:05:41 -0500

    Included the java file for about us page

---
+++ Codes/petschwewy/app/src/main/java/com/example/petsandroid/AboutUsActivity.java
@@ -0,0 +1,17 @@
+package com.example.petsandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+
+
+    }
+
+    }
