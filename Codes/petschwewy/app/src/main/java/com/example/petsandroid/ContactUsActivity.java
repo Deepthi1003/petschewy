2b95a75b7c4270a801e0de64bc3deae00f013c88
Deepthi Chokka <dtchokka@gmail.com>
2020-10-04 22:47:59 -0500

    Included the java file for contact-us page

--- Codes/petschwewy/app/src/main/java/com/example/petsandroid/ContactUsActivity.java
+++ Codes/petschwewy/app/src/main/java/com/example/petsandroid/ContactUsActivity.java
@@ -12,21 +12,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 public class ContactUsActivity extends AppCompatActivity {
-   Button button;
-
+   Button button1,button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactus);
 
-        button = (Button) findViewById(R.id.buttonCall);
+        button1 = (Button) findViewById(R.id.buttonCall);
+        button2=(Button) findViewById(R.id.buttonEmail);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:+19255239060"));
-
                 if (ActivityCompat.checkSelfPermission(ContactUsActivity.this,
                         Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                     return;
@@ -35,8 +34,21 @@ public class ContactUsActivity extends AppCompatActivity {
             }
         });
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                String[] recipients={"dtchokka@gmail.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Questions to be Answered");
+                intent.putExtra(Intent.EXTRA_TEXT,"Kindly write your query here...");
+                intent.putExtra(Intent.EXTRA_CC,"dtchokka@gmail.com");
+                intent.setType("text/html");
+                intent.setPackage("com.google.android.gm");
+                startActivity(Intent.createChooser(intent, "Send mail"));
+            }
+        });
 
     }
 }
